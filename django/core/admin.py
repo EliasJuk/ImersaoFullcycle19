2fb2658de66c2d1df79c5839001e02403505a83b
eliasjuk <eliasp.juk@hotmail.com>
2024-10-24 19:47:05 -0300

    add form ao admin

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from core.models import Video, Tag, VideoMedia
 
+class VideoMediaInline(admin.StackedInline):
+  model = VideoMedia
+  verbose_name = 'Mídia'
+  max_num = 1
+  min_num = 1
+  can_delete = False
+
+class VideoAdmin(admin.ModelAdmin):
+  inlines = [VideoMediaInline]
+
 # Register your models here.
-admin.site.register(Video)
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Tag)
 admin.site.register(VideoMedia)
