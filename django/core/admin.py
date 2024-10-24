b8a369245af9022c798088e279b45c1732e6c9d3
eliasjuk <eliasp.juk@hotmail.com>
2024-10-24 19:49:54 -0300

    reverter exemplo

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from core.models import Video, Tag, VideoMedia
 
-class VideoMediaInline(admin.StackedInline):
-  model = VideoMedia
-  verbose_name = 'Mídia'
-  max_num = 1
-  min_num = 1
-  can_delete = False
+#class VideoMediaInline(admin.StackedInline):
+#  model = VideoMedia
+#  verbose_name = 'Mídia'
+#  max_num = 1
+#  min_num = 1
+#  can_delete = False
 
 class VideoAdmin(admin.ModelAdmin):
-  inlines = [VideoMediaInline]
+  #inlines = [VideoMediaInline]
+  pass
 
 # Register your models here.
-admin.site.register(Video, VideoAdmin)
+admin.site.register(Video)
 admin.site.register(Tag)
 admin.site.register(VideoMedia)
