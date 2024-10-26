b48df09d4fc64ddc12871a47ecc969b8fed6fee1
eliasjuk <eliasp.juk@hotmail.com>
2024-10-26 17:54:22 -0300

    colors

--- django/core/services.py
+++ django/core/services.py
@@ -1,4 +1,8 @@
-class VideoService:
+# Cores
+YELLOW = "\033[33m"
+RESET = "\033[0m"  # Reseta a cor
+
+class VideoService: 
 
 	def process_upload(self, video_id: int, chunk_index: int, chunk: bytes):
-		print(f'Processing upload for video {video_id}, chunk {chunk_index}...')
+		print(f'{YELLOW}Processing upload for video {video_id}, chunk {chunk_index}...{RESET}')
