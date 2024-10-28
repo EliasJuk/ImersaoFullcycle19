8c2cbdcb44c29ed1b5d74e661357d4033d40a05a
eliasjuk <eliasp.juk@hotmail.com>
2024-10-28 18:40:16 -0300

    script

--- golang/copiar_arquivos.sh
+++ golang/copiar_arquivos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Script para copiar arquivos da pasta uploads para o diretório /media
+# Script para copiar arquivos da pasta uploads para o diretório /media/uploads/
 
 cp -r /app/mediatest/media/uploads/* /media/uploads/
