fd76af2d03d710549567e29a63e6b90c3e352837
eliasjuk <eliasp.juk@hotmail.com>
2024-10-29 15:51:32 -0300

    Fix: indepotency.go - Query

--- golang/internal/converter/indepotency.go
+++ golang/internal/converter/indepotency.go
@@ -9,7 +9,7 @@ import (
 
 func IsProcessed(db *sql.DB, videoID int) bool {
 	var isProcessed bool
-	query := "SELECT EXIST(SELECT 1 FROM processed_video wehre video_id = $1 and status='success')"
+	query := "SELECT EXIST(SELECT 1 FROM processed_video where video_id = $1 and status='success')"
 	err := db.QueryRow(query, videoID).Scan(&isProcessed)
 	if err != nil {
 		slog.Error("Error checking if video is processed", slog.Int("video_id", videoID)) //slog.Error(err)
@@ -19,7 +19,7 @@ func IsProcessed(db *sql.DB, videoID int) bool {
 }
 
 func MarkProcessed(db *sql.DB, videoID int) error {
-	query := "Insert info processed_video (video_id, status, processed_at) value ($1, $2, $3)"
+	query := "Insert into processed_videos (video_id, status, processed_at) values($1, $2, $3)"
 	_, err := db.Exec(query, videoID, "success", time.Now())
 	if err != nil {
 		slog.Error("Error marking video as proccess", slog.Int("video_id", videoID)) //slog.String("error", err.String())
@@ -30,7 +30,7 @@ func MarkProcessed(db *sql.DB, videoID int) error {
 
 func RegisterError(db *sql.DB, errorData map[string]any, err error) {
 	serializedError, _ := json.Marshal(errorData)
-	query := "insert into process_errors_log (error_details, created_at) values($1 $2)"
+	query := "insert into process_errors_log (error_details, created_at) values($1, $2)"
 	_, dbErr := db.Exec(query, serializedError, time.Now())
 	if dbErr != nil {
 		slog.Error("Error registering error", slog.String("error_details", string(serializedError)))
